02ebda3500121a16c92e075587e120f8ee2e6906
doko37 <hunubmm@gmail.com>
2021-02-18 21:00:23 +1300

    changed target=_blank to rel=noopener

--- src/Components/ContactUs/Information/InformationItem/InformationItem.js
+++ src/Components/ContactUs/Information/InformationItem/InformationItem.js
@@ -4,7 +4,7 @@ import './InformationItem.css';
 const informationItem = (props) => (
     <li className="InformationItem">
         <img className="Icon" src={props.icon} alt="icon" width="20rem" height="20rem"/>
-        <span><a className={props.haveLink ? "Link" : null}href={props.haveLink ? props.link : null} target="_blank">{props.children}</a></span>
+        <span><a className={props.haveLink ? "Link" : null}href={props.haveLink ? props.link : null} rel="noopener">{props.children}</a></span>
     </li>
 )
 
