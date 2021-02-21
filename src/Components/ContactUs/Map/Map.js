cc4c02e10177b0059b43ad5fce67c44c9b491197
doko37 <hunubmm@gmail.com>
2021-02-22 04:29:28 +1300

    fixed CI problem

--- src/Components/ContactUs/Map/Map.js
+++ src/Components/ContactUs/Map/Map.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {GoogleMap, useJsApiLoader, Marker} from '@react-google-maps/api';
+import {GoogleMap, useJsApiLoader} from '@react-google-maps/api';
 
 import './MapContainer.css';
 
