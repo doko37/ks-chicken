8f83a56364de7ee50fb0c0da458e1397d5dcde69
doko37 <hunubmm@gmail.com>
2021-03-05 14:53:33 +1300

    added icon, title and slideshow in home page

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import FullContainer from './Components/Containers/FullContainer';
+import headerlogo from './headerlogo.png';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
+      <link rel="icon" href ={headerlogo} />
       <div className="BarTop"/>
       <div className="BarBottom"/>
-        <div className="App">
-          <FullContainer />
-        </div>
+      <div className="App">
+        <FullContainer />
+      </div>
       </BrowserRouter>
     );
   }
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,14 +1,37 @@
 import React from 'react';
+import {Slide} from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css';
 import './Home.css';
 import HomeBottomHalf from './HomeBottomHalf/HomeBottomHalf';
 import image1 from './HomeButtons/Images/homepagechicken.jpeg';
-
+import image2 from './HomeButtons/Images/image2.PNG';
+import image3 from './HomeButtons/Images/image3.PNG';
+import image4 from './HomeButtons/Images/image4.png';
 
 const home = () => {
+    const properties = {
+        transitionDuration: 500,
+        pauseOnHover: true,
+        easing: 'ease',
+    }
+
     return (
         <div className="Home">
-            <div className="SlideShow">
-                <img className="HomePicture" src={image1} alt="bigboy" />
+            <div className="slide-container">
+                <Slide {...properties}>
+                    <div className="each-slide">
+                        <img className="img1" src={image1} alt="img1"/>
+                    </div>
+                    <div className="each-slide">
+                        <img className="img1" src={image2} alt="img2"/>
+                    </div>
+                    <div className="each-slide">
+                        <img className="img1" src={image3} alt="img3"/>
+                    </div>
+                    <div className="each-slide">
+                        <img className="img1" src={image4} alt="img4"/>
+                    </div>
+                </Slide>
             </div>
             <HomeBottomHalf />
         </div>
